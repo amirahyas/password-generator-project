e1db932b24b131d81b8f0b754a1ea9ecb12f9526
amirahyas <143109513+amirahyas@users.noreply.github.com>
2023-10-10 09:08:21 -0400

    push repo

--- Develop/script.js
+++ Develop/script.js
@@ -1,92 +1,89 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-var confirmlength = "";
-var confirLowercase;
-var confirUppercase;
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+var confirmLength = 0;
+var confirmLowercase;
+var confirmUppercase;
 var confirmNumeric;
 var confirmSpecial;
 
 
-
-// Var Arrays
+//Var Arrays
 var Uppercase= ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O.P,Q,R,S,T,U,V,W,X,Y,Z,"];
 var Lowercase = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
-var number = ["0,1,2,3,4,5,6,7,8,9"];
+var numeric = ["0,1,2,3,4,5,6,7,8,9"];
 var special = ["!,@,#,$,%,^,&,*,(,),_,+"];
 
-// Prompt that confirms user is inside the parameters
+//Prompt that confirms user is inside the parameters
 function generatePassword() {
-  var confirmLnegth=("You must chose between 8 and 128 characters")
-  console.log("confirm length" + confirmLength)
-}
-
-  // Password parameters
-  var confirmLowercase = confirm("Click 'ok' to include lowercase characters");
-  var confirmUppercase = confirm("Click 'ok' to include Uppercase characters");    
-  var confirmSpecial = confirm("Click 'ok' to include special characters");
-  var confirmNumeric = confirm("Click 'ok' to include numeric characters");
-
-  
-    // Loop if password length is outside of parameters
-    var confirmlength= (prompt("Enter the length of the password (8-128 characters):"));
-    if (NaN(length) || length <8 || length > 128) {
+    confirmLength=(prompt("You must chose between 8 and 128 characters"));
+    //Loop if password length is outside of parameters
+    if (isNaN(confirmLength) || confirmLength <8 || confirmLength > 128) {
       alert ("Please enter a valid password length between 8 and 128 characters.");
-      var confirmLength = (prompt("Enter the length of the password between 8 and 128 characters."));
+      confirmLength = (prompt("Enter the length of the password between 8 and 128 characters."));
   
     }
-    // Loop if password doesn't meet criteria
-  while(confirmLowercase === false && confirmUpper === false && confirmNumeric === false && confirmSpecial === false) {
-  alert("You must use one of the parameters");
+    // Returm how many charactes the user will have  
+    alert(`Your password will have ${confirmLength} characters`);
 
-  var confirmSpecial = confirm("Click 'ok' to include special characters");
-  var confirmNumeric = confirm("Click 'ok' include numeric characters");    
-  var confirmLowercase = confirm("Click 'ok' to include lowercase characters");
-  var confirmUppercase = confirm("Click 'ok' to include uppercase characters"); 
+    //Password parameters
+    confirmLowercase = confirm("Click ok to confirm if you would like to include lowercase characters");
+    confirmUppercase = confirm("Click 'ok' to confirm if you would like to include uppercase characters");    
+    confirmSpecial = confirm("Click 'ok' to confirm if you would like to include special characters");
+    confirmNumeric = confirm("Click 'ok' to confirm if you would like to include numeric characters");
 
-    // Confirm how many characters user password has  
-    alert(`Your password contains ${confirmLength} characters`);
-
-
-    var password = ""
-
-    if (confirmLowerCase) {
-      password = password.concat(Lower)
-    }
-
-    if (confirmUpperCase) {
-      password = password.concat(Upper)
-    }
-
-    if (confirmNumeric) {
-      password = password.concat(number)
-    }
-
-      if (confirmSpecial) {
-        password = password.concat(special)
-      }
-    }
-      console.log(password)
-
-      var randomPassword = ""
+      // Loop if password is outside of criteria
       
-for (var i = 0; i < confirmLength; i++) 
-{
-randomPassword = randomPassword + password[Math.floor(Math.random() * password.length)];
-console.log(randomPassword)
+      
+ while(confirmLowercase === false && confirmUppercase === false && confirmNumeric === false && confirmSpecial === false) {
+        alert("You must use one of the parameters");
+       confirmLowercase = confirm("Click ok to confirm if you would like to include lowercase characters");
+       confirmUppercase = confirm("Click 'ok' to confirm if you would like to include uppercase characters");    
+       confirmSpecial = confirm("Click 'ok' to confirm if you would like to include special characters");
+       confirmNumeric = confirm("Click 'ok' to confirm if you would like to include numeric characters");
+ }
+    
+       // Assign fix this password to the parameters
+       
+       var password = []
+      
+       if (confirmSpecial) {
+         password = password.concat(special)
+       }
+   
+       if (confirmNumeric) {
+         password = password.concat(numeric)
+       }
+         
+       if (confirmLowercase) {
+         password = password.concat(Lowercase)
+       }
+   
+       if (confirmUppercase) {
+         password = password.concat(Uppercase)
+       }
+   
+         console.log(password)
+         
+         var randomPassword = ""
+      
+         for (var i = 0; i < confirmLength; i++) {
+           randomPassword = randomPassword + password[Math.floor(Math.random() * password.length)];
+           console.log(randomPassword)
+         }
+         return randomPassword;
 
-return randomPassword;
-}
+      
 
-// Write password to the #password input
+
+        }
+
+        // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.innerHTML = password;
 
 }
-document.querySelector("#generate").addEventListener("click", writePassword);
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
